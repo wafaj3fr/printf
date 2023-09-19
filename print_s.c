750c0f5ed4ef35bef83e4000470124483894ae4d
wafaj3fr <wafaagaafar@gmail.com>
2023-09-19 08:48:40 -0700

    updates

--- print_s.c
+++ print_s.c
@@ -10,11 +10,12 @@ int print_s(va_list list)
 {
 	const char *str = va_arg(list, const char *);
 	int count = 0;
+
 	while (*str)
 	{
 		write(1, str, 1);
 		str++;
 		count++;
 	}
-	return count;
+	return (count);
 }
