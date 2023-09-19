f19872db4074c8d1e64aefe24d76d9e089bf98e3
Batman99sd <azozkrg1@gmail.com>
2023-09-19 09:23:33 -0700

    edit

--- print_char.c
+++ print_char.c
@@ -9,6 +9,7 @@
 int print_c(va_list list)
 {
 	char c = va_arg(list, int);
+
 	write(1, &c, 1);
-	return 1;
+	return (1);
 }
--- print_num.c
+++ print_num.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-int print_n(va_list list) {
+int print_n(va_list list)
+{
 	int n = va_arg(list, int);
 	int count = 0;
 	int i;
@@ -8,7 +9,8 @@ int print_n(va_list list) {
 	int num_digits;
 
 	/* Handle negative numbers */
-	if (n < 0) {
+	if (n < 0)
+       	{
 		write(1, "-", 1);
 		n = -n;
 		count++;
@@ -17,14 +19,17 @@ int print_n(va_list list) {
 
 	/* Calculate the number of digits in n */
 	num_digits = 1;
-	while (temp >= 10) {
+
+	while (temp >= 10)
+       	{
 		temp /= 10;
 		num_digits++;
 	}
 
 
 	/* Extract and print each digit */
-	for (i = 0; i < num_digits; i++) {
+	for (i = 0; i < num_digits; i++)
+       	{
 		int digit = n % 10;
 		char digit_char = '0' + digit;
 		write(1, &digit_char, 1);
