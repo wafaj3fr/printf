8b6492bf5f8476b43abc06fee2da9a58cfbb70ec
wafaj3fr <wafaagaafar@gmail.com>
2023-09-19 08:36:01 -0700

    added print_s.c

---
+++ print_s.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_s - prints strings
+ * @list: list of arguments
+ * Return: characters count
+ */
+
+int print_s(va_list list)
+{
+	const char *str = va_arg(list, const char *);
+	int count = 0;
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+		count++;
+	}
+	return count;
+}
